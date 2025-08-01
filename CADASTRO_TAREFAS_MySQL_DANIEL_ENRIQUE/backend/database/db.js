0267cecdbcf3195bb102edd7c43a69965b72dd08
Daniel <daniviana2912@gmail.com>
2025-08-01 12:37:34 -0300

    Add files via upload

--- CADASTRO_TAREFAS_MySQL_DANIEL_ENRIQUE/backend/database/db.js
+++ CADASTRO_TAREFAS_MySQL_DANIEL_ENRIQUE/backend/database/db.js
@@ -1,8 +1,9 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "backend/database/tarefas.sqlite"
+const sequelize = new Sequelize("cadastro_tarefas","tarefas_user","123456",{
+  host: "localhost",
+  dialect: "mysql",
+  timestamp: false
 });
 
 module.exports = sequelize;
--- CADASTRO_TAREFAS_MySQL_DANIEL_ENRIQUE/backend/server.js
+++ CADASTRO_TAREFAS_MySQL_DANIEL_ENRIQUE/backend/server.js
@@ -3,14 +3,22 @@ const app = express();
 const cors = require("cors");
 const tarefaRoutes = require("./routes/tarefaRoutes");
 const sequelize = require("./database/db");
-const Tarefa = require("./models/Tarefa");
 
+//midlleware
 app.use(cors());
 app.use(express.json());
-app.use(tarefaRoutes);
 
-sequelize.sync().then(() => {
-  app.listen(3000, () => {
-    console.log("Servidor rodando em http://localhost:3000");
-  });
-});
+//rotas
+app.use("/tarefas",tarefaRoutes);
+
+//Sincronização com o banco de dados
+sequelize.sync()
+    .then(()=>{
+      console.log('Banco de dados sincronizado com sucesso')
+      app.listen(3000, ()=>{
+        console.log('Servidor rodando em http//localhost')
+      });
+    })
+    .catch((error)=>{
+      console.error('Erro ao conectar ao banco de dados: ',error)
+    });
